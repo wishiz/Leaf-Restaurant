0184ef9ddfc67c8ded79189c5c07f283f9304914
wishiz <eugeniavoloshina@gmail.com>
2020-10-07 13:25:08 +0300

    Updates

---
+++ precache-manifest.5859660b98bfcf4d9000d7972765b901.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9af34588f7d1e798e42ad5b1b286fb",
+    "url": "/Leaf-Restaurant/index.html"
+  },
+  {
+    "revision": "e019b2d4a88db6d4a7c4",
+    "url": "/Leaf-Restaurant/static/css/2.dfcaff33.chunk.css"
+  },
+  {
+    "revision": "1d81bba9c16227344ff2",
+    "url": "/Leaf-Restaurant/static/css/main.c67a47d2.chunk.css"
+  },
+  {
+    "revision": "e019b2d4a88db6d4a7c4",
+    "url": "/Leaf-Restaurant/static/js/2.508b6447.chunk.js"
+  },
+  {
+    "revision": "f176b96c54405754efdb753ad3993282",
+    "url": "/Leaf-Restaurant/static/js/2.508b6447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d81bba9c16227344ff2",
+    "url": "/Leaf-Restaurant/static/js/main.e380dc04.chunk.js"
+  },
+  {
+    "revision": "bc4badbc7bcb18820454",
+    "url": "/Leaf-Restaurant/static/js/runtime-main.b40f15e5.js"
+  },
+  {
+    "revision": "344dfb8e21cb51cbabb2b4a375f7a087",
+    "url": "/Leaf-Restaurant/static/media/about_cheers.344dfb8e.webp"
+  },
+  {
+    "revision": "946ecc1539a440879532e852004917a5",
+    "url": "/Leaf-Restaurant/static/media/about_restaurant.946ecc15.webp"
+  },
+  {
+    "revision": "fed3d3d8634871a2d8d892ef8b696db3",
+    "url": "/Leaf-Restaurant/static/media/about_serve.fed3d3d8.webp"
+  },
+  {
+    "revision": "0561ef7058b4d961447e7a4afa4ef3df",
+    "url": "/Leaf-Restaurant/static/media/about_steak.0561ef70.jpg"
+  },
+  {
+    "revision": "826f2f639371b9261b28895b6237463f",
+    "url": "/Leaf-Restaurant/static/media/about_wine.826f2f63.jpg"
+  },
+  {
+    "revision": "4eb5d2243cd3838059e97c32a9aced90",
+    "url": "/Leaf-Restaurant/static/media/chef_bg.4eb5d224.png"
+  },
+  {
+    "revision": "6c3e4bc7fce55a02a2ec0f9b4d478cf0",
+    "url": "/Leaf-Restaurant/static/media/chef_signature.6c3e4bc7.png"
+  },
+  {
+    "revision": "7f32adc55a7852412a22dc0cc966f3b8",
+    "url": "/Leaf-Restaurant/static/media/footer_sushi.7f32adc5.png"
+  },
+  {
+    "revision": "7360e93c94cbf3c63f807ba8499552e8",
+    "url": "/Leaf-Restaurant/static/media/galleryPic1.7360e93c.jpg"
+  },
+  {
+    "revision": "22c04ed10a45f03e94d03ebcfb2ce074",
+    "url": "/Leaf-Restaurant/static/media/galleryPic10.22c04ed1.jpg"
+  },
+  {
+    "revision": "7638eea113a51fe192538ebd10ef7d99",
+    "url": "/Leaf-Restaurant/static/media/galleryPic2.7638eea1.jpg"
+  },
+  {
+    "revision": "4569bf9f33ae7d816136ad63e554ad83",
+    "url": "/Leaf-Restaurant/static/media/galleryPic3.4569bf9f.jpg"
+  },
+  {
+    "revision": "05ab0f949f1aa90ec87d1ae957034eef",
+    "url": "/Leaf-Restaurant/static/media/galleryPic4.05ab0f94.jpg"
+  },
+  {
+    "revision": "b301b631767a3c5a5304e3a81194e0b4",
+    "url": "/Leaf-Restaurant/static/media/galleryPic5.b301b631.jpg"
+  },
+  {
+    "revision": "b43e88e5d695d7d646df1ef8fa749bfe",
+    "url": "/Leaf-Restaurant/static/media/galleryPic6.b43e88e5.jpg"
+  },
+  {
+    "revision": "ceb9c72329823727bae7652b36c231d7",
+    "url": "/Leaf-Restaurant/static/media/galleryPic7.ceb9c723.jpg"
+  },
+  {
+    "revision": "ba482ae275513d6b9dffb796ced83a80",
+    "url": "/Leaf-Restaurant/static/media/galleryPic8.ba482ae2.jpg"
+  },
+  {
+    "revision": "948b01ed26ed3f7446b42f45b41225c7",
+    "url": "/Leaf-Restaurant/static/media/galleryPic9.948b01ed.jpg"
+  },
+  {
+    "revision": "ae90cf0e7cb1502bfcdd8f706580f7e3",
+    "url": "/Leaf-Restaurant/static/media/home_bg.ae90cf0e.jpg"
+  },
+  {
+    "revision": "bf3327123daee6379de429b4f00f4b98",
+    "url": "/Leaf-Restaurant/static/media/menu_bg.bf332712.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Leaf-Restaurant/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Leaf-Restaurant/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Leaf-Restaurant/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Leaf-Restaurant/static/media/slick.f97e3bbf.svg"
+  }
+]);
