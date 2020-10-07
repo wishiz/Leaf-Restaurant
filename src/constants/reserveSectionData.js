9b7e431aa3ce9b0f8adce4b0824ca04322ef07dd
wishiz <eugeniavoloshina@gmail.com>
2020-10-07 13:16:03 +0300

    Added footer license

--- src/constants/reserveSectionData.js
+++ src/constants/reserveSectionData.js
@@ -1,6 +1,5 @@
-export const GOOGLE_MAPS_API_KEY =
-  'AIzafsdfSyBJcE1eHr5HWsHrxXer3Ac75kbM41LtJUk';
-// AIzaSyBJcE1eHr5HWsHrxXer3Ac75kbM41LtJUk
+export const GOOGLE_MAPS_API_KEY = 'API_key_disabled_in_developer_mode';
+
 export const maltaLatLong = { lat: 35.952291, lng: 14.414902 };
 export const restaurantMarker = { lat: 35.952291, lng: 14.414902 };
 
